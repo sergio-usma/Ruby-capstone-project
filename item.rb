e71aaf366ab0a9b7b1fb3382e65f4858071c5ba3
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-09-19 00:15:33 +0300

    implemented methods in tems class

--- item.rb
+++ item.rb
@@ -11,4 +11,17 @@ class Item
       @archived = false
       @id = rand(1..1000)
     end
+
+    def can_be_archive?
+      current_year = Time.now.year
+
+      return true if current_year - @publish_date.year > 10
+
+      false
+    end
+    def move_to_archive
+      @archived = true if can_be_archive?
+    end
+    
+    
 end  
