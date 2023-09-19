4ddee3db82793ee82a9c6ac4d7e48d04fca4ed36
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-18 19:39:53 -0500

    Update can_be_archived? method and getters

--- classes/books.rb
+++ classes/books.rb
@@ -1,9 +1,9 @@
 require_relative 'item'
-class Books < Item
+class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state, date)
-    super(date)
+  def initialize(publisher, cover_state, publish_date, archived: false)
+    super(publish_date, archived: archived)
     @cover_state = cover_state
     @publisher = publisher
   end
