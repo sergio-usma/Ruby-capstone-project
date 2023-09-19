281cf072e51588c054779d782ed03bb0c59e6a98
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-19 12:39:16 +0300

    fix linter errors

--- classes/movies.rb
+++ classes/movies.rb
@@ -1,10 +1,9 @@
-
 class Movies
-    attr_reader :archived
-    attr_accessor :silent
-  
-    def initialize(args)
-      super(args[:genre], args[:author], args[:source], args[:label], args[:publish_date])
-      @silent = args[:silent]
-    end
+  attr_reader :archived
+  attr_accessor :silent
+
+  def initialize(args)
+    super(args[:genre], args[:author], args[:source], args[:label], args[:publish_date])
+    @silent = args[:silent]
+  end
 end
