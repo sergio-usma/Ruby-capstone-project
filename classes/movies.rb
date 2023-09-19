59b5d545bc0b8fcc52a179c360cc5edfbc817501
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-19 12:36:21 +0300

    Add all movie properties in the constructor method

--- classes/movies.rb
+++ classes/movies.rb
@@ -1,4 +1,10 @@
-# frozen_string_literal: true
 
 class Movies
+    attr_reader :archived
+    attr_accessor :silent
+  
+    def initialize(args)
+      super(args[:genre], args[:author], args[:source], args[:label], args[:publish_date])
+      @silent = args[:silent]
+    end
 end
