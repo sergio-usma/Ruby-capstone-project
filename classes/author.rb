8e68a7058cc51cf5b0c5abd64346bdd77f774bce
Javier Vargas <javierralejandroortga@gmail.com>
2023-09-19 16:28:07 -0400

    Create author class

---
+++ classes/author.rb
@@ -0,0 +1,13 @@
+class Author
+  def initialize(id, first_name, last_name)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    @item.label(self)
+  end
+end
