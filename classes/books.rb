46b976eaa6c0f7de5c96d80d4bc6240248f0a553
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-18 19:34:07 -0500

    Add Book class properties in constructor

--- classes/books.rb
+++ classes/books.rb
@@ -1,5 +1,15 @@
-class Book < Item
-  def book
-    puts 'This is the book class'
+require_relative 'item'
+class Books < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(publisher, cover_state, date)
+    super(date)
+    @cover_state = cover_state
+    @publisher = publisher
+  end
+
+  def can_be_archived?
+    archived = super
+    archived || @cover_state == 'bad'
   end
 end
