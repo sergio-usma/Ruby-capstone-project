fce20e0a4ca48c7f1ef6c9629e753afe72a85454
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-18 19:56:35 -0500

    Add author method for Books class

--- classes/item.rb
+++ classes/item.rb
@@ -1,9 +1,11 @@
+require 'date'
 class Item
   attr_reader :genre, :author, :source, :label
+  attr_accessor :publish_date, :archived
 
   def initialize(publish_date, archived: false)
     @id = Random.rand(1..1000)
-    @publish_date = publish_date
+    @publish_date = Date.parse(publish_date)
     @archived = archived
   end
 
@@ -22,5 +24,10 @@ class Item
   def label=(label)
     @label = label
     label.items.push(self) unless label.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
   end
 end
