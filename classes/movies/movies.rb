ad4d49b65f0cfa93ffbc4fb99d5b71486e224b64
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-09-19 13:24:45 +0300

    Add movies.rb to movies folder

---
+++ classes/movies/movies.rb
@@ -0,0 +1,15 @@
+require_relative '../movies/classes/item'
+
+class Movies < Item
+  attr_reader :archived
+  attr_accessor :silent
+
+  def initialize(args)
+    super(args[:genre], args[:author], args[:source], args[:label], args[:publish_date])
+    @silent = args[:silent]
+  end
+
+  def can_be_archived?
+    super && @silent == true
+  end
+end
