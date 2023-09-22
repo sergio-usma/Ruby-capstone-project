1890849ee92f613300a2859e4edb1247be4b1fc0
Sergio Usma <sergioandresusma@hotmail.com>
2023-09-22 14:24:52 -0500

    Add push to add_item method

--- classes/author.rb
+++ classes/author.rb
@@ -1,23 +1,24 @@
 class Author
   attr_accessor :first_name, :last_name
-  attr_reader :id, :items
+  attr_reader :items
 
   def initialize(first_name, last_name)
-    @id = rand(1..1000)
+    @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
 
   def add_item(item)
-    @items.push(item)
-    @item.author(self)
+    item.is_a?(item)
+    item.author = self
+    @items << item
   end
 
-  def to_hash
-    {
-      first_name: @first_name,
-      last_name: @last_name
-    }
-  end
+#   def to_hash
+#     {
+#       first_name: @first_name,
+#       last_name: @last_name
+#     }
+#   end
 end
